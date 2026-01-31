df9297d143fff4f4bba29172bd4e81160359bed6
kyle15451 <kyle.kitchener@outlook.com>
2026-01-31 16:56:13 +0200

    added the register conponent as well as the api call

---
+++ src/app/core/services/auth.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private apiUrl = 'http://localhost:3000/auth';
+  constructor(private http: HttpClient) { }
+
+  register(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/createUser`, userData);
+  }
+
+  login(credentials: { email: string; password: string }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+}
